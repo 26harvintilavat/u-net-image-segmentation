5d659b031fbd2653637e99f222b8946174ca86f7
26harvintilavat <tilavatharvin@gmail.com>
2026-01-30 22:51:29 +0530

    Make training loop AMP-safe for CPU and GPU execution

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import pathlib as Path
 import torch
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
@@ -5,17 +6,20 @@ from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
 from model import UNET
+ROOT_DIR = Path(__file__).resolve().parent.parent
 # from utils import (
-#    load_checkpoint,
- #   save_checkpoint,
-  #  get_loaders,
-   # check_accuracy,
-    #save_predictions_as_imgs,
+# load_checkpoint,
+# save_checkpoint,
+# get_loaders,
+# check_accuracy,
+# save_predictions_as_imgs,
 #)
 
 # Hyperparameters etc.
 learning_rate = 1e-4
 device = "cuda" if torch.cuda.is_available() else "cpu"
+use_amp = device == "cuda"
+scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
 batch_size = 16
 num_epochs = 3
 num_workers = 2
@@ -23,5 +27,31 @@ image_height = 160
 image_width = 240
 pin_memory = True
 load_model = True
+train_image_dir = ROOT_DIR/'data'/'processed'/'train'/'images'
+train_mask_dir = ROOT_DIR/'data'/'processed'/'train'/'masks'
+val_image_dir = ROOT_DIR/'data'/'processed'/'val'/'images'
+val_mask_dir = ROOT_DIR/'data'/'processed'/'val'/'masks'
 
+def train_fn(loader, model, optimizer, loss_fn, scaler):
+    loop = tqdm(loader)
 
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(device=device)
+        targets = targets.float().unsqueeze(1).to(device=device)
+
+        # forward
+        with torch.cuda.amp.autocast(enabled=use_amp):
+            predictions = model(data)
+            loss = loss_fn(predictions, targets)
+
+        # backward 
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
