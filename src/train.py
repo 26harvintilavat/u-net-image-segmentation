1bfed3e9390971579c3adafe398e5dc98db51461
26harvintilavat <tilavatharvin@gmail.com>
2026-01-31 12:31:43 +0530

    feat: add albumentations transforms for training and validation

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ from albumentations.pytorch import ToTensorV2
 from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
-from model import UNET
+from src.model import UNET
 ROOT_DIR = Path(__file__).resolve().parent.parent
 # from utils import (
 # load_checkpoint,
@@ -50,8 +50,59 @@ def train_fn(loader, model, optimizer, loss_fn, scaler):
         scaler.step(optimizer)
         scaler.update()
 
+        # update tqdm loop
+        loop.set_postfix(loss=loss.item())
+
+
 def main():
-    pass
+    train_transform = A.Compose(
+        [
+            A.Resize(height=image_height, width=image_width),
+            A.Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.1),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,       
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    val_transform = A.Compose(
+        [
+            A.Resize(height=image_height, width=image_width),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,            
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    model = UNET(in_channels=3, out_channels=1).to(device=device)
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    train_loader, val_loader = get_loaders(
+        train_image_dir,
+        train_mask_dir,
+        val_image_dir,
+        val_mask_dir,
+        batch_size,
+        train_transform,
+        val_transform,
+        num_workers,
+        pin_memory,
+    )
+
+    use_amp = device == "cuda"
+    scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
+    for epoch in range(num_epochs):
+        train_fn(train_loader, model, optimizer, loss_fn, scaler)
+
 
 if __name__ == "__main__":
     main()
