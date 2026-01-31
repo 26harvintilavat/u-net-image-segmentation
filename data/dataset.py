07c0072ab1146474eab4a247c09bd79911c0b01f
26harvintilavat <tilavatharvin@gmail.com>
2026-01-31 12:25:27 +0530

    refactor: rename dataset class for clarity

--- data/dataset.py
+++ data/dataset.py
@@ -3,7 +3,7 @@ from PIL import Image
 from torch.utils.data import Dataset
 import numpy as np
 
-class CarvanaDataset(Dataset):
+class OxfordIIITPetDataset(Dataset):
     def __init__(self, image_dir, mask_dir, trasnform=None):
         self.image_dir = image_dir
         self.mask_dir = mask_dir
